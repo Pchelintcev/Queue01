c8c14819df3171c828a7f62131b80d7ab8d6c295
Сергей Пчелинцев <s.pchelintcev@yandex.ru>
2022-08-17 14:49:16 +0300

    готовая для проверки

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,35 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        //создаём персоны, через класс Person
-        Person Misha = new Person("Михаил", "Салтыков-Щедрин", 6);
-        Person Boris = new Person("Борис", "Пастернак", 7);
-        Person Vanya = new Person("Иван", "Бунин", 5);
-        Person Sasha = new Person("Александр", "Блок", 4);
-        Person Lev = new Person("Лев", "Толстой", 8);
+    static List<Person> formList() { //TODO формируем список
 
-        //создаем список ""russian"
+        Person Misha = new Person("Русский", "Михаил", "Салтыков-Щедрин", 4);
+        Person Boris = new Person("Немецкий", "Эрих Мария", "Ремарк", 5);
+        Person Vanya = new Person("Французский", "Антуан", "де Сент-Экзюпери", 3);
+        Person Sasha = new Person("Итальянский", "Умберто", "Эко", 2);
+        Person Lev = new Person("Американский", "Эрнест", "Хемингуэй", 6);
+
         ArrayList<Person> russian = new ArrayList<>();
-        //добавляем в список персоны
+
         russian.add(Misha);
         russian.add(Boris);
         russian.add(Vanya);
         russian.add(Sasha);
         russian.add(Lev);
 
-        //делаем очередь "queue" и добавляем в очередь всех из списка "русские"
-        Queue<Person> queue = new LinkedList<>(russian);
+        return russian;
+    }
 
+    public static void main(String[] args) {
+        Queue<Person> queue = new LinkedList<>(formList());
 
-        while (!queue.isEmpty()) { // пока в очереди есть кто-то
-            Person s = queue.poll(); // s = посмотреть кто следующий
-            System.out.println(s); // вывести s на экран
+        while (!queue.isEmpty()) {
+            Person p = queue.poll();
+            if (p.tickets > 0) {
+                System.out.println(p + ", билетов осталось " + p.spendTicket());
+                queue.offer(p);
+            }
         }
-        System.out.println(queue.isEmpty()); //очередь пуста?
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,24 @@
 public class Person {
+    protected String nation;
     protected String name;
     protected String surname;
     protected int tickets;
 
-    public Person(String name, String surname, int tickets) {
+    public Person(String nation, String name, String surname, int tickets) {
+        this.nation = nation;
         this.name = name;
         this.surname = surname;
         this.tickets = tickets;
     }
-    public String toString(){
-        return "Русский писатель " + name +" "+ surname +": "+ tickets + " билетов на карусель.";
+
+    public int spendTicket() {
+        if (tickets > 0) {
+            tickets--;
+        }
+        return tickets;
+    }
+
+    public String toString() {
+        return nation + " писатель " + name + " " + surname + " прокатился на карусели";
     }
 }
